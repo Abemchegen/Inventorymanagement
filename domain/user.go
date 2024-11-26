67ded8a6241848f4dc255aa1f3885b789cb6e046
Abemchegen <88394629+Abemchegen@users.noreply.github.com>
2024-11-26 12:17:40 +0300

    updated roles

--- domain/user.go
+++ domain/user.go
@@ -4,5 +4,5 @@ type User struct {
 	ID       string `json:"id" bson:"_id"`            // Unique identifier
 	Username string `json:"username" bson:"username"` // Login username
 	Password string `json:"password" bson:"password"` // Hashed password
-	Role     string `json:"role" bson:"role"`         // User role (e.g., admin, inventory_manager, sales_staff)
+	Role     string `json:"role" bson:"role"`         // User role (e.g., admin, inventory, warehouse)
 }
